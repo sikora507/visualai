365f92908a76691d178b0eb1bb106a3db6dcdbaa
Tomasz Sikora <sikora507@gmail.com>
2018-03-05 19:51:51 +0100

    things

---
+++ ClientApp/components/counter/counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a Vue.js component.</p>
+
+        <p>Current count: <strong>{{ currentcount }}</strong></p>
+
+        <button @click="incrementCounter">Increment</button>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    export default {
+        data: function () {
+            return {
+                currentcount: 0
+            }
+        },
+        methods: {
+            incrementCounter: function () {
+                this.currentcount++;
+            }
+        }
+    }
+</script>
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Hello from './app/hello.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-var vm = new Vue({
-    el: '#app',
-    render: h => h(Hello),
-    components: { Hello }
-})
