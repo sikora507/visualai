992535666a771009e211c62618abf576c5473c36
Tomasz Sikora <sikora507@gmail.com>
2018-03-06 17:53:14 +0100

    preview component

--- ClientApp/components/activationFunctions/previewcomponent.vue
+++ ClientApp/components/activationFunctions/previewcomponent.vue
@@ -1,8 +1,12 @@
 ﻿<template>
     <div class="function">
         <h3>{{name}}</h3>
-        <button class="btn btn-default">Compute</button>
-        <div class="preview"></div>
+        <button class="btn btn-default" @click="computeFunction">
+            Compute
+        </button>
+        <div class="preview">
+            <canvas id="canv"></canvas>
+        </div>
     </div>
 </template>
 <script>
@@ -14,10 +18,31 @@
             return {
 
             }
+        },
+        methods: {
+            computeFunction: function () {
+                alert('computing');
+            }
+        },
+        mounted: function () {
+            var c = this.$el.getElementsByTagName('canvas')[0];
+            var ctx = c.getContext("2d");
+            var width = c.offsetWidth;
+            var height = c.offsetHeight;
+            ctx.canvas.width = width;
+            ctx.canvas.height = height;
+            ctx.translate(0.5, 0.5)
+            ctx.setLineDash([4, 8]);
+            ctx.beginPath();
+            ctx.moveTo(0, height / 2);
+            ctx.lineTo(width, height / 2);
+            ctx.moveTo(width / 2, 0);
+            ctx.lineTo(width / 2, height);
+            ctx.stroke();
         }
     }
 </script>
-<style>
+<style scoped>
     .function {
         position: relative;
         text-align: center;
@@ -27,7 +52,6 @@
         border-radius: 10px;
         overflow: hidden;
     }
-
     .preview {
         background-color: cornsilk;
         flex-grow: 0;
@@ -38,5 +62,11 @@
         position: absolute;
         top: 15px;
         right: 15px;
+    }
+    canvas{
+        background-color: white;
+        width: 350px;
+        height: 150px;
+        margin-top:10px;
     }
 </style>
